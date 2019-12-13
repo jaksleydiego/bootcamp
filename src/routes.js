09bffb918adc56259338626301f439ea370b7b23
diego gonçalves <jaksleydiego@gmail.com>
2019-12-13 17:56:08 -0400

    feature: upload avatar image

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,16 @@
 // importando o Router a classe do express
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
+
+import FileController from './app/controllers/FileController';
 // instanciando a rota
 const routes = new Router();
-
+const upload = multer(multerConfig);
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
@@ -14,5 +18,8 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
+routes.post('/files', upload.single('file'), FileController.store);
+
 // exporta as rotas
+
 export default routes;
